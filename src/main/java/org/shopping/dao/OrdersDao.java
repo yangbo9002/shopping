32058c4e5d68690daf31d18fb4db084f5afbcd35
chenyanhui123 <1596274637@qq.com>
2018-04-05 15:55:49 +0800

    订单

--- src/main/java/org/shopping/dao/OrdersDao.java
+++ src/main/java/org/shopping/dao/OrdersDao.java
@@ -1,7 +1,8 @@
 package org.shopping.dao;
 
+import org.shopping.pojo.OrderGoods;
 import org.shopping.pojo.Orders;
 
-public interface OrdersDao extends BaseDao<Orders>{
+public interface OrdersDao extends BaseDao<OrderGoods>{
 
 }
